417420dcc3ddc18a4a8aeba16c23b73f6a35af4c
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-15 00:57:30 +0100

    Added getWeatherData async function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-const hello = 'Hello World';
-console.log(hello);
+// A function that hits the API and returns data
+async function getWeatherData(location) {
+  let response = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      location +
+      '&appid=b25577cea775f772e6801248daec3619',
+    { mode: 'cors' },
+  );
+  let responseObj = await response.json();
+  console.log(responseObj);
+}
+getWeatherData('Ilorin');
