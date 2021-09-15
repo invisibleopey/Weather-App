22634b46003453ab01a3c18a6ae32ccd7dc8b154
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-15 01:25:06 +0100

    Resolved ESLint issues and imported regenerator-runtime

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+import 'regenerator-runtime';
 // A function that hits the API and returns data
 async function getWeatherData(location) {
-  let response = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-      location +
-      '&appid=b25577cea775f772e6801248daec3619',
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b25577cea775f772e6801248daec3619`,
     { mode: 'cors' },
   );
-  let responseObj = await response.json();
+  const responseObj = await response.json();
   console.log(responseObj);
 }
 getWeatherData('Ilorin');
